eaa3ef5941dc79778305f4ccd56b7d4c020c2369
hantrungkien <hantrungkien98@gmail.com>
2020-09-12 01:20:52 +0700

    add web

---
+++ Web Demo Frontend/common.js
@@ -0,0 +1,40 @@
+function validate(){
+    window.addEventListener('load', function() {
+   
+        var forms = document.getElementsByClassName('needs-validation');
+       
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+           
+          }, false);
+
+        });
+      }, false);
+    };
+    validate();
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
