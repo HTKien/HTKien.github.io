87ce9534a380b3c29d65c11e691f3cafc2a014af
hantrungkien <hantrungkien98@gmail.com>
2020-11-09 03:39:06 +0700

    add basic

--- HCI_05/js/common.js
+++ HCI_05/js/common.js
@@ -1,7 +1,23 @@
-
 // document.getElementById('hocgo').onclick=function(a){
 //     document.getElementById('trangchu').classList.remove("active");
 
 //     this.classList.add("active");
 
 // }
+function xemCachGo() {
+    if (document.getElementById("xemCachGoText").text === "Xem cách gõ") {
+        document.getElementById("xemCachGoText").innerHTML = "Quay lại";
+        document.getElementById("imgLuyenGo").setAttribute("src", "img/tocky-removebg-preview.png");
+
+    } else
+    if (document.getElementById("xemCachGoText").text === "Quay lại") {
+        document.getElementById("xemCachGoText").innerHTML = "Xem cách gõ";
+        document.getElementById("imgLuyenGo").setAttribute("src", "img/banphim-removebg-preview.png");
+
+    }
+}
+
+function playGame() {
+    document.getElementById("imgPlayGame").setAttribute("src", "img/game2.png");
+
+}
