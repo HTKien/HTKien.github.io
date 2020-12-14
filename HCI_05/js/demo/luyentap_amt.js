a09530119ae1166496306c76bd703a820044fbaa
hantrungkien <hantrungkien98@gmail.com>
2020-12-14 20:16:53 +0700

    kien

--- HCI_05/js/demo/luyentap_amt.js
+++ HCI_05/js/demo/luyentap_amt.js
@@ -5,6 +5,8 @@ document.getElementById("btn-cach-go").onclick = function() {
         document.getElementById("219").style.backgroundColor = "#FFB700";
         document.getElementById("219_").classList.remove("hover__");
         document.getElementById("219_").style.backgroundColor = "#FFB700";
+        document.getElementById("ban-tay").src = "img/amT2.png";
+
     } else
     if (document.getElementById("btn-cach-go").textContent === "Quay lại") {
         document.getElementById("btn-cach-go").textContent = "Xem cách gõ";
@@ -12,6 +14,8 @@ document.getElementById("btn-cach-go").onclick = function() {
         document.getElementById("219").style.backgroundColor = "#333333";
         document.getElementById("219_").classList.add("hover__");
         document.getElementById("219_").style.backgroundColor = "#333333";
+        document.getElementById("ban-tay").src = "img/amS_null.png";
+
     }
 
 }
